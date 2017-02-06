d95c386b43101abd5959604c95f8ffdf1877cb56
Nicholas Capel <nrjc2@cam.ac.uk>
2017-02-06 14:58:06 +0000

    Changed delay to be closer together

--- scenarios/cartDoublePendulum/testNewStabilizeAlg.m
+++ scenarios/cartDoublePendulum/testNewStabilizeAlg.m
@@ -18,7 +18,7 @@
 % Jonas Umlauft, Rowan McAllister 2015-07-01
 clear all;
 errormag = [1];
-delay = [0.049 0.5 0.051];
+delay = [0.04999 0.5 0.05001];
 for errornum = 1
 	for delaynum = 1:3
 	    close all;
