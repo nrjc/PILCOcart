6202b039114ed1d0f20ea21ae5d6332acdbe1ed9
Nicholas Capel <nrjc2@cam.ac.uk>
2017-02-07 10:50:24 +0000

    test

--- base/odestep.m
+++ base/odestep.m
@@ -39,7 +39,7 @@ end
 if isfield(plant, 'delay')
   [T y] = ode45(plant.ode, linspace(dt-plant.delay,dt,3), x0, OPTIONS, u{1});
   udt = u{1}(dt);
-  u = u(1:ulength-1);
+  u = u(2:ulength);
   u{ulength} = @(t)ctrltype(t, f, udt);  
   [T y] = ode45(plant.ode, linspace(0,dt-plant.delay,3), y(3,:)', OPTIONS, u{1});     
 else
