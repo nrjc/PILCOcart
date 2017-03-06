9a3a1f37a23b332eecbe99b2c1049093833e28f8
Nicholas Capel <nrjc2@cam.ac.uk>
2017-03-06 13:45:04 +0000

    Added new test for seq of linear controller

--- scenarios/cartDoublePendulum/delayTest/viewrollout.m
+++ scenarios/cartDoublePendulum/delayTest/viewrollout.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 setdir;
-load ./swingup/CartDoubleSwingupRestart93_H60.mat
+load ./swingup/CartDoubleSwingupFixedWeight100_H60.mat
 plotall;
 currentrun = j+1;
 animate(latent(currentrun), data(currentrun), dt, cost);
