2c99ff232073ce52ecf25e41113dc7aefa327a31
Nicholas Capel <nrjc2@cam.ac.uk>
2017-03-11 19:24:25 +0000

    Part 3 of sequence training

--- scenarios/cartDoublePendulum/delayTest/viewrollout.m
+++ scenarios/cartDoublePendulum/delayTest/viewrollout.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 setdir;
-load ./swingup/CartDoubleSwingupLinearSeqShort2p27_H20.mat
+load ./swingup/CartDoubleSwingupLinearSeqShort2p77_H20.mat
 plotall;
 currentrun = j+1;
 animate(latent(currentrun), data(currentrun), dt, cost);
