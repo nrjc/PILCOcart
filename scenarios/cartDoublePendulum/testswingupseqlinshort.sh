8663cc167cae2dd96665a261297ddb282ffce133
Nicholas Capel <nrjc2@cam.ac.uk>
2017-03-14 11:57:19 +0000

    New second part of swing experiment

--- scenarios/cartDoublePendulum/testswingupseqlinshort.sh
+++ scenarios/cartDoublePendulum/testswingupseqlinshort.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 matlab_exec=/misc/apps/matlab/matlabR2015b/bin/matlab
 batchnum=7
-nohup ${matlab_exec} -nodesktop -nosplash -nodisplay -r "doitSwingupLinearSeqShort;exit;" > nohup${batchnum}.out 2>&1&
+nohup ${matlab_exec} -nodesktop -nosplash -nodisplay -r "doitSwingupLinearSeqShort2;exit;" > nohup${batchnum}.out 2>&1&
