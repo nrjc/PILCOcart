e71285c5713b8ac6637485e208fd7500a4d344c5
Nicholas Capel <nrjc2@cam.ac.uk>
2017-03-16 21:19:17 +0000

    Dynamic length

--- scenarios/cartDoublePendulum/delayTest/viewrollout.m
+++ scenarios/cartDoublePendulum/delayTest/viewrollout.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 setdir;
-load ./swingup/CartDoubleSwingupLinearSeqShort2p70_H30.mat
+load ./CartDoubleSwingupRestart15_H60.mat
 plotall;
 currentrun = j+1;
 animate(latent(currentrun), data(currentrun), dt, cost);
--- scenarios/cartDoublePendulum/testuprightstabilize.sh
+++ scenarios/cartDoublePendulum/testuprightstabilize.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 matlab_exec=/misc/apps/matlab/matlabR2015b/bin/matlab
 batchnum=1
-nohup ${matlab_exec} -nodesktop -nosplash -nodisplay -r "doitSwingup;exit;" > nohup${batchnum}.out 2>&1&
+nohup ${matlab_exec} -nodesktop -nosplash -nodisplay -r "doitSwingupDynamicLength;exit;" > nohup${batchnum}.out 2>&1&
