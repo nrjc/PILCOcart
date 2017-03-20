c97c13100f8e836e2cdd1e7f57b50c94d9e4c50a
Nicholas Capel <nrjc2@cam.ac.uk>
2017-03-20 13:40:44 +0000

    Removed delay bug

--- scenarios/cartDoublePendulum/runStabilizationDelay.m
+++ scenarios/cartDoublePendulum/runStabilizationDelay.m
@@ -1,6 +1,6 @@
 batchnum=20;
 for error = [0.25 1 2 4]
-    for delay = [0.07 0.08 0.09 0.10]
+    for delay = [0.07 0.08 0.09 0.095]
         doitStabilizeTiming(delay,error,batchnum);
         batchnum=batchnum+1;
     end
--- sshdept.sh
+++ sshdept.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 jump_host="nrjc2@gate.eng.cam.ac.uk"
 local_path="./scenarios/cartDoublePendulum/delayTest"
-destination_path="~/PILCOcart/scenarios/cartDoublePendulum/*39*"
+destination_path="~/PILCOcart/scenarios/cartDoublePendulum/*72*"
 host="dirichlet"
 host2="nrjc2@dirichlet"
 scp -o ProxyCommand="ssh $jump_host nc $host 22" $host2:$destination_path $local_path 
