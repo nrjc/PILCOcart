50a679216666ae3250ac2d9afa762ac28c13ef89
Nicholas Capel <nrjc2@cam.ac.uk>
2017-03-21 13:42:01 +0000

    Make run trial MK3 compatible with my build

--- myExps/pendcode/runtrialMK3.sh
+++ myExps/pendcode/runtrialMK3.sh
@@ -2,20 +2,20 @@
 
 # rm *txt
 if [ $1 = "0" ]; then
-	scp cbl-fs:"~/ctrl/scenarios/cartPole/centers.txt ~/ctrl/scenarios/cartPole/weights.txt ~/ctrl/scenarios/cartPole/W.txt" ./
+	cp ~/PILCOcart/scenarios/cartPole/centers.txt ~/PILCOcart/scenarios/cartPole/weights.txt ~/PILCOcart/scenarios/cartPole/W.txt ./
 	while [ ! -f centers.txt ] || [ ! -f weights.txt ] || [ ! -f W.txt ]
 	do
 	    sleep 50
 	    #echo -n '.'
-	    scp cbl-fs:"~/ctrl/scenarios/cartPole/centers.txt ~/ctrl/scenarios/cartPole/weights.txt ~/ctrl/scenarios/cartPole/W.txt" ./
+		cp ~/PILCOcart/scenarios/cartPole/centers.txt ~/PILCOcart/scenarios/cartPole/weights.txt ~/PILCOcart/scenarios/cartPole/W.txt ./
 	done
-	ssh cbl-fs 'rm ~/ctrl/scenarios/cartPole/*txt'
+	rm ~/PILCOcart/scenarios/cartPole/*txt
 fi
 
 ./autosingleMK3
-scp state.txt cbl-fs:~/ctrl/scenarios/cartPole/
+cp state.txt ~/PILCOcart/scenarios/cartPole/
 
-rm -R ../benchdata/single/$2
-mkdir ../benchdata/single/$2
-mv *.txt ../benchdata/single/$2
-mv test.avi ../benchdata/single/$2
+rm -R ~/benchdata/single/$2
+mkdir ~/benchdata/single/$2
+mv *.txt ~/benchdata/single/$2
+mv test.avi ~/benchdata/single/$2
