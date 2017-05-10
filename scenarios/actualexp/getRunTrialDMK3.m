0201fc7721fbf07f0a73bd480bf71c11adfd053d
Nicholas Capel <nrjc2@cam.ac.uk>
2017-05-10 16:47:30 +0100

    Fix get and set trial

--- scenarios/actualexp/getRunTrialDMK3.m
+++ scenarios/actualexp/getRunTrialDMK3.m
@@ -5,7 +5,8 @@ end
 
 %load the trial
 nstate=load('state.txt');
-newH = min([size(nstate,1) H+1]);
+%newH = min([size(nstate,1) H+1]);
+newH = size(nstate,1); %Overriding length of data to copy for first state.
 data(J+j).state = nstate(1:newH,1:12);
 data(J+j).action = nstate(1:newH-1,13);
 
--- scenarios/actualexp/setRunTrialDMK3.m
+++ scenarios/actualexp/setRunTrialDMK3.m
@@ -1,25 +1,10 @@
 function setRunTrial(ctrl)
-	%output RBF policy
+	%output Linear Policy
 	format short
-	fileID = fopen('centers.txt', 'w');
-	fprintf(fileID,'%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n',ctrl.policy.p.inputs');
-	fclose(fileID);
-
-	fileID = fopen('W.txt', 'w');
-	fprintf(fileID,'%f\n',ctrl.policy.p.hyp(1:end-2));
-	fclose(fileID);
-
-	nN=length(ctrl.policy.p.target);
-	nD = length(ctrl.policy.p.hyp)-2;
-	X = ctrl.policy.p.hyp;
-	K = zeros(nD,nD); beta = zeros(nD);
-	    
-	inp = bsxfun(@rdivide,ctrl.policy.p.inputs,exp(X(1:nD)'));
-	K = exp(2*X(nD+1)-maha(inp,inp)/2);
-	L = chol(K(:,:) + exp(2*X(nD+2))*eye(nN))';
-	beta = L'\(L\ctrl.policy.p.target);
-
 	fileID = fopen('weights.txt', 'w');
-	fprintf(fileID,'%f\n', beta);
-	%fprintf(fileID,'%f\n', ctrl.policy.p.target);
+	fprintf(fileID,'%f %f %f %f %f %f %f %f %f %f %f %f\n',ctrl.policy.p.w);
+	fclose(fileID);
+
+	fileID = fopen('bias.txt', 'w');
+	fprintf(fileID,'%f\n',ctrl.policy.p.b);
 	fclose(fileID);
