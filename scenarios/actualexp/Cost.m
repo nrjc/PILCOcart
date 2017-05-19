3e72d8ce4d3b6003784ffc1549af5bbc541f7d20
Nicholas Capel <nrjc2@cam.ac.uk>
2017-05-19 14:35:43 +0100

    Correct the cost function

--- scenarios/actualexp/Cost.m
+++ scenarios/actualexp/Cost.m
@@ -18,7 +18,7 @@ classdef Cost < CostSuper
   
   properties (SetAccess = private)
     % default values:
-    ell = [0.46 0.84]
+    ell = [0.70 0.84]
     width = 0.8
     gamma = 1.0    % no discounting
   end
